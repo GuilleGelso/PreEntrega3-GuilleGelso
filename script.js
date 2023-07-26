ef156b7e40a6de1cb4a6d4e3b29ae9f4268e1afa
Guille Gelso <celidiog@gmail.com>
2023-07-26 16:05:15 -0300

    PROYECTO FINAL

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
+
 function miProgramaPrincipal() {
-  let productos = [
-    { id: 2, nombre: "CPU I3", categoria: "COMPONENTES", stock: 6, precio: 35490, rutaImagen: "prod1.webp" },
-    { id: 5, nombre: "DISCO SSD 240", categoria: "Almacenamiento", stock: 9, precio: 5250, rutaImagen: "prod2.webp" },
-    { id: 7, nombre: "DISCO 2TB", categoria: "Almacenamiento", stock: 6, precio: 3200, rutaImagen: "prod3.webp" },
-    { id: 9, nombre: "DISCO EXTERNO 1TB", categoria: "Almacenamiento", stock: 22, precio: 1700, rutaImagen: "prod4.webp" },
-    { id: 12, nombre: "ROUTER TP-LINK", categoria: "CONECTIVIDAD", stock: 2, precio: 3600, rutaImagen: "prod5.webp" },
-    { id: 15, nombre: "ROUTER MERCUSYS", categoria: "CONECTIVIDAD", stock: 5, precio: 7900, rutaImagen: "prod6.webp" },
-    { id: 17, nombre: "MICRO SD", categoria: "Almacenamiento", stock: 6, precio: 6352, rutaImagen: "prod7.webp" },
-  ]
+
+  const urlLocal = '/db.json';
+  let productos=[]
+ fetch(urlLocal)
+.then(response => response.json())
+.then(data => {productos = data.productos
+  console.log(productos)
+
+
 
   let carritoJSON = JSON.parse(localStorage.getItem("carrito"))
   let carrito = carritoJSON ? carritoJSON : []
-
+  
   let contenedor = document.getElementById("contenedor")
   renderizar(productos, contenedor, carrito)
   renderizarCarrito(carrito)
-
+}
+)
   let botonFinalizarCompra = document.getElementById("finalizarCompra")
   botonFinalizarCompra.addEventListener("click", () => finalizarCompra(carrito))
+  
+  
 }
 
 miProgramaPrincipal()
 
-function finalizarCompra(carrito) {
+function finalizarCompra() {
   let carritoFisico = document.getElementById("carrito")
   carritoFisico.innerHTML = ""
   localStorage.removeItem("carrito")
   carrito = []
-  renderizarCarrito([])
+  localStorage.clear()
+  fincompra()
+  
 }
 
 
@@ -35,20 +41,21 @@ function renderizar(arrayDeElementos, contenedor, carrito) {
 
   contenedor.innerHTML = ""
 
-  arrayDeElementos.forEach(({ nombre, rutaImagen, stock, id }) => {
+  arrayDeElementos.forEach(({ nombre, rutaImagen,  id }) => {
     let tarjetaProducto = document.createElement("div")
 
     tarjetaProducto.classList.add("tarjetaProducto")
     tarjetaProducto.innerHTML = `
       <h2 class="card-text">${nombre} </h2>
       <div class=imagen style="background-image: url(../multimedia/${rutaImagen})"></div>
-      <p>Quedan ${stock} unidades</p>
+     
       <button id=${id} class="btn btn-primary mb-3">Agregar al carrito</button>
     `
     contenedor.appendChild(tarjetaProducto)
     let botonAgregarAlCarrito = document.getElementById(id)
     botonAgregarAlCarrito.addEventListener("click", () => agregarAlCarrito(arrayDeElementos, id, carrito))
   })
+  
 }
 
 function agregarAlCarrito(arrayDeElementos, id, carrito) {
@@ -67,6 +74,7 @@ function agregarAlCarrito(arrayDeElementos, id, carrito) {
       subtotal: productoBuscado.precio
     })
   }
+  compranotifi()
   localStorage.setItem("carrito", JSON.stringify(carrito))
   renderizarCarrito(carrito)
 }
@@ -103,4 +111,32 @@ function mostrarOcultar() {
   let carrito = document.getElementById("contenedorCarrito")
   padreContenedor.classList.toggle("oculto")
   carrito.classList.toggle("oculto")
+}
+
+
+function compranotifi(){
+  Toastify({
+    text: "Producto agregado correctamente",
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", 
+    position: "right", 
+    stopOnFocus: true, 
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function(){} 
+  }).showToast();
+}
+
+function fincompra(){
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Gracias por tu compra',
+    showConfirmButton: false,
+    timer: 1500
+  })
 }
